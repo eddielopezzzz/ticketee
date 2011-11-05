031ded55e58ec70b359ff2dafa8463b53a62d52f
Eddie Lopez <eddielopezzzz@hotmail.com>
2011-11-05 16:07:17 -0500

    Added title functionality for show page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,10 @@
 module ProjectsHelper
+  
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee").join(" - ") unless parts.empty?
+      end
+    end
+  end
 end
