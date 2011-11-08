85534392f7a590fe02749fb7fcd315707fcc2657
Eddie Lopez <eddielopezzzz@hotmail.com>
2011-11-07 20:37:45 -0600

    Added updating projects functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,15 +10,30 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(params[:project])
     if @project.save
-      flash[:notice] = "Project has been created"
-      redirect_to @project
+    flash[:notice] = "Project has been created"
+    redirect_to @project
     else
-      flash[:alert] = "Project has not been created."
-      render :action => "new"
+    flash[:alert] = "Project has not been created."
+    render :action => "new"
     end
   end
-
+  
   def show
     @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+    flash[:notice] = "Project has been updated."
+    redirect_to @project
+    else
+    flash[:alert] = "Project has not been updated"
+    render :action => "edit"
+    end
   end
 end
--- features/support/factories.rb
+++ features/support/factories.rb
@@ -1,3 +1,4 @@
+Kristian1
 Dir[Rails.root + "factories/*.rb"].each do |file|
   require file
 end
