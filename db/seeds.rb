83e0dbab39f528889177deb12cf9f2da6e858ea4
Eddie Lopez <eddielopezzzz@hotmail.com>
2011-12-01 23:29:42 -0600

    When updating a comment's status, it also updates the ticket's status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
-admin_user = User.create(:email => "admin@ticketee.com", :password => "password")
+#admin_user = User.create(:email => "eddielopezzzz@hotmail.com", :password => "Kristian1")
 
-admin_user.admin = true
-admin_user.confirm!
+#admin_user.admin = true
+#admin_user.confirm!
 
-Project.create(:name => "Ticketee Beta")
+#Project.create(:name => "blah")
+
+State.create(:name => "New",
+             :background => "#85FF00",
+             :color => "white")
+             
+State.create(:name => "Open",
+             :background => "#00CFFD",
+             :color => "white")
+             
+State.create(:name => "Closed",
+             :background => "black",
+             :color => "white")
