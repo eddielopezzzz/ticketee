3df28f49e9f66ef0007221d076567c83f5f569b5
eddielopezzzz <eddielopezzzz@hotmail.com>
2011-12-06 00:14:38 -0600

    Users can add tags when adding a comment

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -5,7 +5,8 @@ module TicketsHelper
         if comment.previous_state && comment.state != comment.previous_state
           "#{render comment.previous_state} &rarr; #{render comment.state }".html_safe
         else
-          "&rarr;" + render(comment.state).html_safe
+          #"&rarr;" + render(comment.state)
+          "#{render comment.state }".html_safe
         end
       end
     end
